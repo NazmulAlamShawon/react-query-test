c190f7c6697a397463e650a5ca36bb8476ed847c
Nazmul Alam <shawonkst15@gmail.com>
2023-10-18 15:25:46 +0600

    add file

---
+++ src/Product.jsx
@@ -0,0 +1,14 @@
+import { useState,useEffect } from "react"
+
+const Product = () => {
+  const [products,setProducts] = useState([])
+useEffect(() =>{
+  
+},[])
+
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
